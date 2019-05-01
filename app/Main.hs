915059e5e93386fdb389c42801bc9abc031cb9c3
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-05-01 11:44:38 +0900

    C-c interruption

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,19 @@
 module Main where
 
+import System.Console.Haskeline
 
 main :: IO ()
-main = putStrLn "nyaan"
+main =
+  let
+    loop :: InputT IO ()
+    loop = handle (\Interrupt -> loop) $ do
+      minput <- getInputLine "% "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just input -> do
+          outputStrLn $ "Input was: " ++ input
+          loop
+  in do
+    runInputT defaultSettings (withInterrupt loop)
+
